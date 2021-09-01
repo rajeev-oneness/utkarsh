133afa02212cd78ee19ca51b63fba14a69ec3650
Rajeev <rrpit9@gmail.com>
2021-09-01 19:37:00 +0530

    View

--- storage/framework/views/ca9e2413a151143e8993dbae6b8b9a0b157b1e29.php
+++
@@ -1,5 +0,0 @@
-<?php echo $__env->make("site.partials.header", \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-
-<?php $__env->startSection("content"); ?>
-<?php echo $__env->yieldSection(); ?>
-<?php echo $__env->make("site.partials.footer", \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /home1/demo9tbi/utkarshelectricals.in/resources/views/site/partials/app.blade.php ENDPATH**/ ?>
--- storage/framework/views/f857cca25f51c64f90e435f38a8512f67ff57d82.php
+++
@@ -1,5 +0,0 @@
-<?php echo $__env->make("site.partials.header", \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-
-<?php $__env->startSection("content"); ?>
-<?php echo $__env->yieldSection(); ?>
-<?php echo $__env->make("site.partials.footer", \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /home/demo9dtx/public_html/dev/utkarsh/resources/views/site/partials/app.blade.php ENDPATH**/ ?>
